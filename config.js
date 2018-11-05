e4572935c9714e44f9a2a12b7a55b10eee6d1c28
kenneth okenwa <okenwanonsokenneth@gmail.com>
2018-11-05 17:13:49 +0100

    update config.js
    deleted access token keys and secret token keys

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'rx0GOqfsZCFXZ5DDYkWGr4zDH',
-    consumer_secret: 'KAm53TpqbIJ3V785ZiQc0UBCqJwvxFC75IqnLviugDhW08BEee',
-    access_token: '1052141636363452416-9WW6XCQm6KX5TXzMszZPTrVXifpLam',
-    access_token_secret: 'cdc8iurZ8ztKMQDhmsYo6OtuCeSWaSIvEZ5g27mA518yd',
+    consumer_key: 'consumer key here',
+    consumer_secret: 'secret token here',
+    access_token: 'access token',
+    access_token_secret: 'access_token here',
 }
